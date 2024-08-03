76505c38fa27417bfae65268467601de47882e89
invictus1032 <invictus1032@gmail.com>
2024-08-03 07:16:23 +0700

    add example components (words)
    red and blue

---
+++ src/routes/example/red/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    import '../../../app.css'
+</script>
+<main>
+    <h1 class="word">
+        Red
+    </h1>
+</main>
+<style>
+    main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        margin: 0;
+        font-family: Arial, sans-serif;
+        text-align: center;
+    }
+    .word {
+        font-size: 40vh;
+        color: blue;
+    }
+</style>
